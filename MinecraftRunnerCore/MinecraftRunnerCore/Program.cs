8daff3f08b906ce0fd0ddfaa8117291f8773ba21
Itszuvalex <Itszuvalex@gmail.com>
2020-01-23 01:17:30 -0800

    Setup .netcore 3.1 project

---
+++ MinecraftRunnerCore/MinecraftRunnerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinecraftRunnerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
