b5b123c114fc22c65d1c55725157754fe54a5c81
Itszuvalex <Itszuvalex@gmail.com>
2020-01-24 02:50:28 -0800

    Working without Async

---
+++ MinecraftRunnerCore/MinecraftRunnerCore/MinecraftRunner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MinecraftRunnerCore
+{
+    class MinecraftRunner
+    {
+        private Task LoopTask { get; set; }
+        private CancellationTokenSource LoopToken { get; set; }
+
+        public bool ActiveLoop => LoopTask != null;
+
+        public void StartLoop()
+        {
+            CloseLoop();
+            InstallIfNecessary();
+
+            LoopToken = new CancellationTokenSource();
+            LoopTask = Task.Factory.StartNew(delegate
+            {
+                while (!LoopToken.Token.IsCancellationRequested)
+                {
+                    MainLoopIteration();      
+                }
+                Console.WriteLine("Task Cancelled");
+            }, LoopToken.Token, TaskCreationOptions.LongRunning);
+        }
+
+        public void StopLoop()
+        {
+            CloseLoop();
+        }
+
+        private void CloseLoop()
+        {
+            if (LoopTask == null) return;
+
+            LoopToken?.Cancel();
+            LoopToken?.Dispose();
+            LoopToken = null;
+            LoopTask = null;
+        }
+
+        private void MainLoopIteration()
+        {
+        }
+
+        private void InstallIfNecessary()
+        {
+
+        }
+    }
+}
--- MinecraftRunnerCore/MinecraftRunnerCore/Program.cs
+++ MinecraftRunnerCore/MinecraftRunnerCore/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MinecraftRunnerCore
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var cancellationToken = new CancellationTokenSource();
+            var runner = new MinecraftRunner();
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                Console.WriteLine("Cancellation received");
+                runner.StopLoop();
+                e.Cancel = true;
+            });
+
+            runner.StartLoop();
+
+            while (runner.ActiveLoop)
+            { Thread.Sleep(1000); }
+
+            Console.WriteLine("Ended loop");
         }
     }
 }
